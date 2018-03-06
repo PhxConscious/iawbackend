ca08e5728527dad3df450193c3dc88059f06ab36
Ismael Perez <ismaelperezemail@gmail.com>
2018-03-06 14:23:25 -0700

    changed db name to iaw

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     development: {
         client: 'pg',
         connection: {
-            database: 'instAwesome',
+            database: 'iaw',
             host: '127.0.0.1'
         }
     },
