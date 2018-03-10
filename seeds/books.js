bc149259ca7568584239dd2dc81c35ce862a18a1
Dylan Cunningham <gdylanc@gmail.com>
2018-03-10 12:47:40 -0700

    updates seeds

--- seeds/books.js
+++ seeds/books.js
@@ -4,10 +4,321 @@ exports.seed = function(knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('lms_books').insert([
-                {name: "module_1",
-                    module: {"array":[{"title":"Instagram 101","description":"Basic communication: Posts, Stories, and DM's","image":"https://i.imgur.com/EHYS6cp.png","video":"https://www.youtube.com/watch?v=i6G53BMgugo","id":"aaaaa","lessons":[{"title":"Lesson Title 1","description":"Lesson Description 1","video":"https://www.youtube.com/watch?v=i6G53BMgugo","id":"u01l01","questions":[{"id":"u01l01q01","contentType":"checkTasks","title":"Basicsasd","description":"Here is how to perform a Basic Search - find Conscious Creative (@phxconscious)"},{"id":"u01l01q02","contentType":"checkTasks","title":"Basicsfdfd","description":"How to Follow an account - Follow Conscious Creative"},{"id":"u01l01q03","contentType":"checkTasks","title":"Basics3wef","description":"How to view a story"},{"id":"u01l01q04","contentType":"checkTasks","title":"Basicswef","description":"How to comment on a story"},{"id":"u01l01q05","contentType":"checkTasks","title":"Basicser","description":"How to like a post - Like a Consious Creative post"},{"id":"u01l01q06","contentType":"checkTasks","title":"Basicsert","description":"How to Save a post - Save a consious Creative post"},{"id":"u01l01q07","contentType":"checkTasks","title":"Basicshgj","description":"How to Comment - Comment on a Consious Creative post"}]},{"title":"Lesson Title 2","description":"Lesson Description 2","video":"https://www.youtube.com/watch?v=i6G53BMgugo","id":"u01l02","questions":[{"id":"u01l02q01","contentType":"checkTasks","title":"Basics","description":"Here is how to perform a Basic Search - find Conscious Creative (@phxconscious)"},{"id":"u01l02q02","contentType":"checkTasks","title":"Basics","description":"How to Follow an account - Follow Conscious Creative"},{"id":"u01l02q03","contentType":"checkTasks","title":"Basics3","description":"How to view a story"},{"id":"u01l02q04","contentType":"checkTasks","title":"Basics","description":"How to comment on a story"},{"id":"u01l02q05","contentType":"checkTasks","title":"Basics","description":"How to like a post - Like a Consious Creative post"},{"id":"u01l02q06","contentType":"checkTasks","title":"Basics","description":"How to Save a post - Save a consious Creative post"}]}]},{"title":"Full Online Analysis Reports","description":"Full Online Analysis Reports yada yada test missing","image":"https://i.imgur.com/EHYS6cp.png","video":"https://www.youtube.com/watch?v=i6G53BMgugo","id":"bbbbb","lessons":[{"title":"Lesson Title 1","description":"Lesson Description 1","video":"https://www.youtube.com/watch?v=i6G53BMgugo","id":"u02l01","questions":[{"id":"u02l01q01","contentType":"checkTasks","title":"What the report is","description":"Do you feel like you have a good understanding? If not watch it again"},{"id":"u02l01q02","contentType":"checkTasks","title":"How to read the report","description":"wGive us 3 short term (can be accomplished in 3 weeks) milestones"}]}]}]
-                    }
-                }
-            ]);
+         {
+            "name":"module_1",
+            "module":{
+               "array":[
+                  {
+                     "title":"Getting Started",
+                     "description":"",
+                     "image":"https://i.imgur.com/EHYS6cp.png",
+                     "id":"u01",
+                     "lessons":[
+                        {
+                           "title":"Quick tutorial on how to use the platform",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u01l01",
+                           "questions":[
+                              {
+                                 "id":"u01l01q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Do you understand?",
+                                 "description":""
+                              }
+                           ]
+                        }
+                     ]
+                  },
+                  {
+                     "title":"Instagram 101",
+                     "description":"",
+                     "image":"https://i.imgur.com/EHYS6cp.png",
+                     "id":"u02",
+                     "lessons":[
+                        {
+                           "title":"Basics",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u02l01",
+                           "questions":[
+                              {
+                                 "id":"u02l01q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Basic communication: Posts, Stories, and DM's",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q02",
+                                 "contentType":"checkTasks",
+                                 "title":"Here is how to perform a Basic Search - find Conscious Creative (@phxconscious)",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q03",
+                                 "contentType":"checkTasks",
+                                 "title":"How to Follow an account - Follow Conscious Creative",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q04",
+                                 "contentType":"checkTasks",
+                                 "title":"How to view a story",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q05",
+                                 "contentType":"checkTasks",
+                                 "title":"How to comment on a story",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q06",
+                                 "contentType":"checkTasks",
+                                 "title":"How to like a post - Like a Conscious Creative post",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q07",
+                                 "contentType":"checkTasks",
+                                 "title":"How to save a post - Save a Conscious Creative post",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q08",
+                                 "contentType":"checkTasks",
+                                 "title":"How to Comment - Comment on a Conscious Creative post",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l01q09",
+                                 "contentType":"checkTasks",
+                                 "title":"How to Like a comment - like another comment on a Conscious Creative post",
+                                 "description":""
+                              }
+                           ]
+                        },
+                        {
+                           "title":"Connecting Your Ecosystem",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u02l02",
+                           "questions":[
+                             {
+                                "id":"u02l02q01",
+                                "contentType":"checkTasks",
+                                "title":"Connecting your Facebook page to your profile",
+                                "description": ""
+                             },
+                             {
+                                "id":"u02l02q02",
+                                "contentType":"checkTasks",
+                                "title":"Connecting Twitter",
+                                "description":""
+                             },
+                             {
+                                "id":"u02l02q03",
+                                "contentType":"checkTasks",
+                                "title":"Connecting Tumbler (if you have it)",
+                                "description":""
+                             },
+                             {
+                                "id":"u02l02q04",
+                                "contentType":"checkTasks",
+                                "title":"The power of Inviting your Facebook friends. Have you invited a few or all of your Facebook friends?",
+                                "description":""
+                             }
+                           ]
+                        },
+                        {
+                           "title":"Profiles",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u02l03",
+                           "questions":[
+                              {
+                                 "id":"u02l03q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Profile image - do you have an image uploaded?",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l03q02",
+                                 "contentType":"checkTasks",
+                                 "title":"Check your username. Is it in line with your brand and other social accounts?",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l03q03",
+                                 "contentType":"checkTasks",
+                                 "title":"Check to see how the info from your Facebook page shows up on your Instagram profile.",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l03q04",
+                                 "contentType":"textArea",
+                                 "title":"Compose your Bio here, copy and paste it in your bio in instagram AND SUBMIT IT HERE FOR THE EXPERTS REVIEW.",
+                                 "description":"How to comment on a story"
+                              },
+                              {
+                                 "id":"u02l03q05",
+                                 "contentType":"checkTasks",
+                                 "title":"Review your profile, is everything working and updated?",
+                                 "description":""
+                              }
+                           ]
+                        },
+                        {
+                           "title":"Direct Messages",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u02l04",
+                           "questions":[
+                              {
+                                 "id":"u02l04q01",
+                                 "contentType":"checkTasks",
+                                 "title":"How to find them and how do they work. Send us a Direct Message.",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u02l04q02",
+                                 "contentType":"checkTasks",
+                                 "title":"Best practices. Do you understand?",
+                                 "description":""
+                              }
+                           ]
+                        }
+                     ]
+                  },
+                  {
+                     "title":"Full Online Analysis Reports",
+                     "description":"",
+                     "image":"https://i.imgur.com/EHYS6cp.png",
+                     "id":"u03",
+                     "lessons":[
+                        {
+                           "title":"What The Report Is",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u03l01",
+                           "questions":[
+                              {
+                                 "id":"u03l01q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Do you feel like you have a good understanding? If not watch it again",
+                                 "description":""
+                              }
+                           ]
+                        },
+                        {
+                           "title":"How To Read The Report",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u03l02",
+                           "questions":[
+                              {
+                                 "id":"u03l02q01",
+                                 "contentType":"textArea",
+                                 "title":"Write your first (of three) milestone that can be accomplished in 3 weeks",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u03l02q02",
+                                 "contentType":"textArea",
+                                 "title":"Write your second (of three) milestone that can be accomplished in 3 weeks",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u03l02q03",
+                                 "contentType":"textArea",
+                                 "title":"Write your last milestone that can be accomplished in 3 weeks",
+                                 "description":""
+                              }
+                           ]
+                        }
+                     ]
+                  },
+                  {
+                     "title":"Content",
+                     "description":"",
+                     "image":"https://i.imgur.com/EHYS6cp.png",
+                     "id":"u04",
+                     "lessons":[
+                        {
+                           "title":"What Is Good Content?",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u04l01",
+                           "questions":[
+                              {
+                                 "id":"u04l01q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Is this clear?",
+                                 "description":""
+                              }
+                           ]
+                        },
+                        {
+                           "title":"Your Content",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u04l02",
+                           "questions":[
+                              {
+                                 "id":"u04l02q01",
+                                 "contentType":"textInput",
+                                 "title":"Do you have a content bank now? Paste a link to dropbox, box, or other cloud based folder link... or something",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u04l02q02",
+                                 "contentType":"checkTasks",
+                                 "title":"Here is how you find your invite to our content structure & organize your content. Do you understand?",
+                                 "description":""
+                              }
+                           ]
+                        },
+                        {
+                           "title":"Content Creation",
+                           "description":"",
+                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                           "id":"u04l03",
+                           "questions":[
+                              {
+                                 "id":"u04l03q01",
+                                 "contentType":"checkTasks",
+                                 "title":"Do you feel confident creating the different types of content?",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u04l03q02",
+                                 "contentType":"checkTaskscheckTasks",
+                                 "title":"Images",
+                                 "description":""
+                              },{
+                                 "id":"u04l03q03",
+                                 "contentType":"checkTasks",
+                                 "title":"Video",
+                                 "description":""
+                              },
+                              {
+                                 "id":"u04l03q04",
+                                 "contentType":"checkTasks",
+                                 "title":"Graphics",
+                                 "description":""
+                              },{
+                                 "id":"u04l03q05",
+                                 "contentType":"checkTasks",
+                                 "title":"How to run a content creation workshop. Do you understand?",
+                                 "description":""
+                              }
+                           ]
+                        }
+                     ]
+                  },
+               ]
+            }
+         }
+      ]);
         });
 };
