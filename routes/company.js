228e49eb9a9ec6ca30e471d249711a851f7a183e
Ismael Perez <ismaelperezemail@gmail.com>
2018-03-11 17:05:32 -0700

    set up put route for user_company_join table

--- routes/company.js
+++ routes/company.js
@@ -20,21 +20,41 @@ router.get('/:id', function (req, res, next) {
 });
 
 //Post new company to database
-router.post('/new', function (req, res, next) {
+router.post('/new/:company_id', function (req, res, next) {
     knex('company_table')
         .insert({
+            company_id: req.params.company_id,
             company_name: req.body.company_name,
             company_email: req.body.company_email,
             company_phone: req.body.company_phone,
             brand_id: req.body.brand_id,
             company_goals: req.body.company_goals,
-            company_content_creator: req.body.user_progress,
+            company_content_creator: req.body.company_content_creator,
             company_username: req.body.company_username,
             company_password: req.body.company_password
         })
         .returning("*")
         .then(data => {
-            knex('company_table').select().where("company_phone", req.body.company_phone).then(company => res.send(company))
+            knex('company_table').select().where("company_id", req.params.company_id).then(company => res.send(company))
+        })
+});
+
+router.put('/new/:company_id', function (req, res, next) {
+    knex('company_table')
+        .update({
+            company_id: req.params.company_id,
+            company_name: req.body.company_name,
+            company_email: req.body.company_email,
+            company_phone: req.body.company_phone,
+            brand_id: req.body.brand_id,
+            company_goals: req.body.company_goals,
+            company_content_creator: req.body.company_content_creator,
+            company_username: req.body.company_username,
+            company_password: req.body.company_password
+        })
+        .returning("*")
+        .then(data => {
+            knex('company_table').select().where("company_id", req.params.company_id).then(company => res.send(company))
         })
 });
 
