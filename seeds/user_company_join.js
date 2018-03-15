2894f7aa6dd72fe4b10005750833f7881260d798
Dylan Cunningham <gdylanc@gmail.com>
2018-03-15 16:21:24 -0700

    update fixture data

--- seeds/user_company_join.js
+++ seeds/user_company_join.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
         .then(function () {
             return knex('user_company_join').insert([
                 {
-                    firebase_id: "rkMC3u3Yhjc90rLDj2Sim418qoC2",
+                    firebase_id: "sarahtheclient",
                     company_id: '1234567890'
                 }
             ]);
