f4520358c5dd00512dcdd4be81840f1b41f392f2
Dylan Cunningham <gdylanc@gmail.com>
2018-03-17 09:29:10 -0700

    fixes route and a couple of seed updates

--- routes/company.js
+++ routes/company.js
@@ -74,13 +74,15 @@ router.post('/new/:company_id', function (req, res, next) {
         })
         .returning("*")
         .then(data => {
-            knex('company_table').select().where("company_id", req.params.company_id).then(company => res.send(company))
+          knex("company_table").select("*").where("company_id", req.params.company_id).then(company => {
+            res.send(company)
+          })
         })
 });
 
 router.put('/new/:company_id', function (req, res, next) {
     console.log("company", req.body)
-    knex('company_table')
+    knex('company_table').select().where("company_id", req.params.company_id)
         .update({
             company_id: req.params.company_id,
             company_name: req.body.company_name,
@@ -126,21 +128,5 @@ router.put('/new/:company_id', function (req, res, next) {
     })
 })
 
-
-// router.post('/new', function (req, res, next) {
-//     knex.raw(`insert into company_table (company_name, company_email, company_phone, brand_id, company_goals, company_content_creator, company_username, company_password ) values (
-//     '${req.body.company_name}',
-//     '${req.body.company_email}',
-//     '${req.body.company_phone}',
-//     '${req.body.brand_id}',
-//     '${req.body.company_goals}',
-//     '${req.body.company_content_creator}',
-//     '${req.body.company_username}',
-//     '${req.body.company_password}'
-//     )`)
-//         .then(() => {
-//             res.send('success')
-//         })
-// });
 
 module.exports = router;
