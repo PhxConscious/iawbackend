abc5b7ac10d43c0006ab4a1c182448d79fc3e11f
Dylan Cunningham <gdylanc@gmail.com>
2018-03-16 18:01:45 -0700

    no message

--- seeds/books.js
+++ seeds/books.js
@@ -8,28 +8,43 @@ exports.seed = function(knex, Promise) {
             "name":"module_1",
             "module":{
                "array":[
-                  {
-                     "title":"Getting Started",
-                     "description":"",
-                     "image":"https://i.imgur.com/EHYS6cp.png",
-                     "id":"u01",
-                     "lessons":[
-                        {
-                           "title":"Quick tutorial on how to use the platform",
-                           "description":"",
-                           "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
-                           "id":"u01l01",
-                           "questions":[
-                              {
-                                 "id":"u01l01q01",
-                                 "contentType":"checkTasks",
-                                 "title":"Do you understand?",
-                                 "description":""
-                              }
-                           ]
-                        }
-                     ]
-                  },
+                 {
+                    "title":"Getting Started",
+                    "description":"",
+                    "image":"https://i.imgur.com/EHYS6cp.png",
+                    "id":"u01",
+                    "lessons":[
+                       {
+                          "title":"Quick tutorial on how to use the platform",
+                          "description":"",
+                          "video":"https://www.youtube.com/watch?v=i6G53BMgugo",
+                          "id":"u01l01",
+                          "questions":[
+                             {
+                                "id":"u01l01q01",
+                                "indexOfCorrect": [0],
+                                "contentType":"multiChoice",
+                                "title":"What's the best way to send us feedback?",
+                                "description":"",
+                                "buttonText": "finish unit 1",
+                                "correctResponse": "That's right! We look forward to hearing your ideas.",
+                                "incorrectResponse": "Using the Profile dropdown will always result in faster response times from us.",
+                                "questions": [
+                                   {
+                                      "text": "From the Profile dropdown",
+                                      "isCorrect": true
+                                   },
+                                   {
+                                     "text": "Via email or Instagram DM",
+                                     "isCorrect": false
+                                   }
+                                ]
+
+                             }
+                          ]
+                       }
+                    ]
+                 },
                   {
                      "title":"Instagram 101",
                      "description":"",
