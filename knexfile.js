dba146091aba390c85255fc6b2331f23bd046fdf
cjannenga <cjannenga312@gmail.com>
2018-03-21 10:48:43 -0700

    Added the PSQL data to the KnexFile for production and staging Databases

--- knexfile.js
+++ knexfile.js
@@ -12,33 +12,21 @@ module.exports = {
     staging: {
         client: 'postgresql',
         connection: {
-            database: 'my_db',
-            user:     'username',
-            password: 'password'
+            database: 'de4fse0jv85blu',
+            host: 'postgres://gnqsaqtswopemg:716876673327f8d575cf12aebda0b47864a475c432fbceecfc31103da4910190@ec2-107-20-233-240.compute-1.amazonaws.com:5432/de4fse0jv85blu',
+            user:     'gnqsaqtswopemg',
+            password: '716876673327f8d575cf12aebda0b47864a475c432fbceecfc31103da4910190'
         },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
     },
 
     production: {
         client: 'postgresql',
         connection: {
-            database: 'my_db',
-            user:     'username',
-            password: 'password'
+            database: 'd9h0ie6n92npam',
+            host: 'postgres://jxjgvcatrgcmdp:10dba1d9748815412145c75b6d6be576548da8c54d81fed4644dc2b14ec33aea@ec2-174-129-225-9.compute-1.amazonaws.com:5432/d9h0ie6n92npam',
+            user:     'jxjgvcatrgcmdp',
+            password: '10dba1d9748815412145c75b6d6be576548da8c54d81fed4644dc2b14ec33aea'
         },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
     }
 
 };
