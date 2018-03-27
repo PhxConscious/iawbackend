4cb4f441427494ccdd5e936fd533e1fa16b07522
cjannenga <cjannenga312@gmail.com>
2018-03-27 16:54:13 -0700

    Added Production DB credentials in attempt to deploy

--- knexfile.js
+++ knexfile.js
@@ -14,10 +14,10 @@ module.exports = {
     production: {
         client: 'postgresql',
         connection: {
-            database: process.env.IAW_DB,
-            host: process.env.IAW_DB_HOST,
-            user: process.env.IAW_DB_USER,
-            password: process.env.IAW_DB_PASSWORD,
+            database: 'df41vd5sndvmes' ,
+            host: 'ec2-54-243-63-13.compute-1.amazonaws.com',
+            user: 'fnpdqbdkrptkdx',
+            password: '4ef3c75ab2537ea02d46f0db4dbcd9b887745e1a57e879c21d9af65d1c455344',
         },
     }
 
