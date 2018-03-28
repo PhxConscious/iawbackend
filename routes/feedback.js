cdbffe4bb6e0c6cebd879f0198d84e7fa0a0efd5
Dylan Cunningham <gdylanc@gmail.com>
2018-03-28 09:32:47 -0700

    Feedback: new routes

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,7 +4,8 @@ let knex = require('../db/knex');
 
 
 router.get('/', function(req, res) {
-    knex("feedback").select()
+    knex("feedback").join("user_table", "feedback.firebase_id", "user_table.firebase_id").select("user_table.first_name", "user_table.last_name", "feedback.comment", "feedback.created_at", "feedback.feedback_id", "feedback.parent_id", "feedback.firebase_id")
+      .where("parent_id", null)
       .then(data => res.send(data))
 });
 
@@ -13,6 +14,12 @@ router.get('/user/:firebase_id', function(req, res) {
     .where("firebase_id", req.params.firebase_id)
     .then(data => res.send(data))
 });
+
+router.get('/parent/:parent_id', function(req, res, next) {
+  knex("feedback").join("user_table", "feedback.firebase_id", "user_table.firebase_id").select("user_table.first_name", "user_table.last_name", "feedback.comment", "feedback.created_at", "feedback.feedback_id", "feedback.parent_id", "feedback.firebase_id")
+    .where("parent_id", req.params.parent_id)
+    .then(data => res.send(data))
+})
 
 router.get('/comment/:feedback_id', function(req, res) {
   knex("feedback").select()
@@ -24,11 +31,20 @@ router.post('/new/:firebase_id', function(req, res) {
   knex('feedback')
     .insert({
       comment: req.body.comment,
-      firebase_id: req.params.firebase_id
+      firebase_id: req.params.firebase_id,
+      parent_id: req.body.parent_id
     })
     .returning("*")
     .then(data => {
-      res.send(data)
+      let theId = data[0].feedback_id
+      knex("feedback").join("user_table", "feedback.firebase_id", "user_table.firebase_id").select("user_table.first_name", "user_table.last_name", "feedback.comment", "feedback.created_at", "feedback.feedback_id", "feedback.parent_id", "feedback.firebase_id")
+      .where({
+        feedback_id: theId
+      })
+      .then(data => {
+        res.send(data)
+      })
+
     })
 })
 
