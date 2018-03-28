6d77ae2941fb421f5e77299d6a45ed110f74ff3d
cjannenga <cjannenga312@gmail.com>
2018-03-27 17:30:42 -0700

    Revert changes back to original

--- knexfile.js
+++ knexfile.js
@@ -4,20 +4,20 @@ module.exports = {
     development: {
         client: 'pg',
         connection: {
-            database:  'iaw',
-            host:  '127.0.0.1',
-            user: '',
-            password:  '',
+            database: process.env.IAW_DB || 'iaw',
+            host: process.env.IAW_DB_HOST || '127.0.0.1',
+            user: process.env.IAW_DB_USER || '',
+            password: process.env.IAW_DB_PASSWORD || '',
         }
     },
 
     production: {
         client: 'postgresql',
         connection: {
-            database: 'df41vd5sndvmes' ,
-            host: 'ec2-54-243-63-13.compute-1.amazonaws.com',
-            user: 'fnpdqbdkrptkdx',
-            password: '4ef3c75ab2537ea02d46f0db4dbcd9b887745e1a57e879c21d9af65d1c455344',
+            database: process.env.IAW_DB,
+            host: process.env.IAW_DB_HOST,
+            user: process.env.IAW_DB_USER,
+            password: process.env.IAW_DB_PASSWORD,
         },
     }
 
