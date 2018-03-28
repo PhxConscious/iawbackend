7912350ac9e746262851f855b0fc600c11253ef6
cjannenga <cjannenga312@gmail.com>
2018-03-27 17:14:16 -0700

    Heroku Tests

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
     development: {
         client: 'pg',
         connection: {
-            database: process.env.IAW_DB || 'iaw',
-            host: process.env.IAW_DB_HOST || '127.0.0.1',
-            user: process.env.IAW_DB_USER || '',
-            password: process.env.IAW_DB_PASSWORD || '',
+            database:  'iaw',
+            host:  '127.0.0.1',
+            user: '',
+            password:  '',
         }
     },
 
