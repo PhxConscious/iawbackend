e768b600afc1f28d5a4def6fb3b90162084d6955
Dylan Cunningham <gdylanc@gmail.com>
2018-04-05 22:09:25 -0700

    update fixture data seeds

--- seeds/expert_user.js
+++ seeds/expert_user.js
@@ -6,7 +6,8 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('expert_user_join').insert([
         {user_id: "sarahtheclient", expert_id:"bobtheexpert"},
-        {user_id: "francoistheclient", expert_id:"bobtheexpert"}
+        {user_id: "francoistheclient", expert_id:"bobtheexpert"},
+        {user_id:"sYW2qp9RKkPgSPPjmaEWo4SlgCn1", expert_id:"bobtheexpert"}
       ]);
     });
 };
